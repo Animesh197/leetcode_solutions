18017a71c3721b643311e74823b759a4e0e50ea6
Animesh Kumar Rai <animeshrai197@gmail.com>
2026-08-16 15:41:51 +0530

    Time: 43 ms (88.55%), Space: 30.4 MB (95.42%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt = [0, 0, 0]
+        for x in stones:
+            cnt[x % 3] += 1
+
+        c0, c1, c2 = cnt
+
+        if c1 == 0 or c2 == 0:
+            return max(c1, c2) > 2 and c0 % 2 == 1
+
+        return abs(c1 - c2) > 2 or c0 % 2 == 0
