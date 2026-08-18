7ee6489bc3ff9fafa4ed88c26ad6c4a2275ec896
Animesh Kumar Rai <animeshrai197@gmail.com>
2026-08-18 22:15:02 +0530

    Time: 5 ms (40.57%), Space: 19.1 MB (99.18%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestInteger(self, nums: List[int], k: int) -> int:
+        count = [0] * 51
+        
+        for i in range(len(nums) - k + 1):
+            seen = set()
+            
+            for j in range(i, i + k):
+                seen.add(nums[j])
+            
+            for x in seen:
+                count[x] += 1
+        
+        for x in range(50, -1, -1):
+            if count[x] == 1:
+                return x
+        
+        return -1
